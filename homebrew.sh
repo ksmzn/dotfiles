544f54a65096cfab64c529e98d9947cf6c51b527
naoto_koshimizu <naoto_koshimizu@ALB0273.local>
2016-03-31 16:03:38 +0900

    delete

--- homebrew.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ `uname` = "Darwin" ]; then
-#     java -version
-#     xcode-select --install
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#     brew doctor
-#     sh brewfile.sh
-fi
-
